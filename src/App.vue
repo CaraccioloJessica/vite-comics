c717c3b7fa14ff15b5e2432ff9c299d657c52941
Jessica <jesscara.993@gmail.com>
2022-12-12 16:53:16 +0100

    header, nav base structure and style

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,8 @@ export default {
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/_variables' as *;
+
+body {
+  background-color: $black_dc;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,9 +10,70 @@ export default {
 </script>
 
 <template>
-  <h1>Prova</h1>
+  <header>
+    <div class="container">
+      <a href="#">
+        <img src="/img/dc-logo.png" alt="DC">
+      </a>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Link
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Link
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/_variables' as *;
 
+header {
+  background-color: #fff;
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 90px;
+
+    img {
+      transform: scale(0.55);
+    }
+
+    ul {
+      list-style: none;
+
+      li {
+        display: inline-block;
+        padding: 1rem;
+        font-weight: 600;
+
+        a {
+          color: $gray_dc;
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
+}
 </style>
