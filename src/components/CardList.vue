76c25b016d2e123f62de46fbbd78f7a585c55b9b
Jessica <jesscara.993@gmail.com>
2022-12-13 16:26:44 +0100

    imported CardItem inside CardList component

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,5 +1,11 @@
 <script>
+import CardItem from './CardItem.vue'
+
 export default {
+  components: {
+    CardItem
+  },
+
   data() {
     return {
       comics: [
@@ -85,12 +91,7 @@ export default {
 <template>
   <div class="container">
     <!-- comics card -->
-    <div class="card">
-      <img
-        src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-        alt="">
-      <h3>Titolo</h3>
-    </div>
+    <CardItem />
   </div>
 
 </template>
